adacae6cb1a8814091cf2a92f02246985b8521f8
Asli00 <asliahmed0802@hotmail.com>
2022-07-05 09:43:15 +0100

    second commit:spring

---
+++ src/main/java/com/qa/dfespringboott/DfeSpringBoottApplication.java
@@ -0,0 +1,13 @@
+package com.qa.dfespringboott;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DfeSpringBoottApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DfeSpringBoottApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qa/dfespringboott/DfeSpringBoottApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.dfespringboott;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DfeSpringBoottApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
